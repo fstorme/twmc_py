2e63ce601b12bf0b4321c1420be8469832e777a9
Florent Storme <florentstorme@gmail.com>
2026-01-07 23:07:54 +0100

    cleaning up the interation code

--- main.py
+++ main.py
@@ -27,44 +27,41 @@ def main(h_params: HamiltionanParameters, evol_params: EvolutionParameters):
     n_times = np.round(np.log2((evol_params.t_end - evol_params.t_start) * w_max))
     dt = (evol_params.t_end - evol_params.t_start) / (2 ** (n_times + 9))
     dt_obs = (evol_params.t_end - evol_params.t_start) / evol_params.n_frames
+    t_vec = evol_params.t_start + dt * np.arange(2 ** (n_times + 9) + 1)
+    frames = range(0, len(t_vec), int(np.floor(dt_obs / dt)))
+    t_obs = t_vec[frames]
+
+    # Rescaling constants
+    u = dt * h_params.u
+    omega = dt * h_params.omega
+    gamma = dt * h_params.gamma
+    f = dt * h_params.f
 
     def f_NL(beta, t):
-        return (
-            1j * h_params.u * (np.abs(beta) ** 2 - 1) * dt * beta + 1j * h_params.f * dt
-        )
+        return 1j * u * (np.abs(beta) ** 2 - 1) * beta + 1j * f
 
+    exp_elt = np.exp((-1j * omega - gamma / 2))
     beta_init = 0
-    beta_tot = np.zeros(
-        (evol_params.n_frames, evol_params.n_config), dtype=np.complex64
-    )
+    beta_tot = np.zeros((len(frames), evol_params.n_config), dtype=np.complex64)
 
     for k in tqdm(range(evol_params.n_config), desc="Evolving trajectories"):
         beta = beta_init + np.sqrt(h_params.gamma * dt / 4) * np.random.normal()
-        t = 0
-        i_step = 0
-        i_frame = 0
-        while t < evol_params.t_end:
+        i_frames = 0
+        for t in t_vec:
             beta_1 = (
-                np.exp((-1j * h_params.omega - h_params.gamma / 2) * dt) * beta
+                exp_elt * beta
                 + f_NL(beta, t)
-                + np.sqrt(h_params.gamma * dt / 4)
-                * (np.random.normal() + 1j * np.random.normal())
+                + np.sqrt(gamma / 4) * (np.random.normal() + 1j * np.random.normal())
             )
 
             beta = (
-                np.exp((-1j * h_params.omega - h_params.gamma / 2) * dt) * beta
-                + 1 / 2 * (f_NL(beta, t) + f_NL(beta_1, t))
-                + np.sqrt(h_params.gamma * dt / 4)
-                * (np.random.normal() + 1j * np.random.normal())
+                exp_elt * beta
+                + 1 / 2 * (f_NL(beta, t) + f_NL(beta_1, t + dt))
+                + np.sqrt(gamma / 4) * (np.random.normal() + 1j * np.random.normal())
             )
-
-            t += dt
-            if i_step % np.floor(dt_obs / dt) == 0:
-                if i_frame <= evol_params.n_frames:
-                    beta_tot[i_frame - 1, k] = beta
-                    i_frame += 1
-
-            i_step += 1
+            if t in t_obs:
+                beta_tot[i_frames, k] = beta
+                i_frames += 1
     return beta_tot
 
 
